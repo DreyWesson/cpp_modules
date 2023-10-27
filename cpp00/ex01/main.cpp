586bce0dcafd7ddb03a671e2002aac855e5f7fe2
Dare Oduwole <doduwole@c2s9d40.42wolfsburg.de>
2023-10-27 22:54:32 +0200

    added specification

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 20:58:53 by doduwole          #+#    #+#             */
-/*   Updated: 2023/10/27 22:26:33 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/10/27 22:53:07 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,27 @@
 // on start-up phonebook should be empty
 // prompt to enter command either ADD, SEARCH, EXIT
 // ADD
-    // prompt input of contact one field at a time
+    // If the user enters this command, they are prompted to input the information
+    // of the new contact one field at a time. Once all the fields have been completed,
+    // add the contact to the phonebook.
+    //     The contact fields are: first name, last name, nickname, phone number, and
+    // darkest secret. A saved contact can’t have empty fields.
+// SEARCH
+    // Display the saved contacts as a list of 4 columns: index, first name, last
+    // name and nickname.
+    // ◦ Each column must be 10 characters wide. A pipe character (’|’) separates
+    // them. The text must be right-aligned. If the text is longer than the column,
+    // it must be truncated and the last displayable character must be replaced by a
+    // dot (’.’).
+    // ◦ Then, prompt the user again for the index of the entry to display. If the index
+    // is out of range or wrong, define a relevant behavior. Otherwise, display the
+    // contact information, one field per line.
+// EXIT
+    // The program quits and the contacts are lost forever
+
+// Any other input is discarded.
+// Once a command has been correctly executed, the program waits for another one. It
+// stops when the user inputs EXIT
 
 int main(void) {
     Contact contact;
